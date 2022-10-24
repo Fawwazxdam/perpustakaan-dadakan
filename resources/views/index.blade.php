656f6b766ac742ecdf082190c1e8f69cf3cf5157
Adam Fawwaz <fawwazadam1005@gmail.com>
2022-10-24 12:14:03 +0700

    6

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -25,9 +25,23 @@
               <li class="nav-item">
                 <a class="nav-link" href="index.php">Book</a>
               </li>
-              <li class="nav-item">
-                <a class="nav-link bg-success text-white rounded-pill" href="login_admin.php">Login</a>
-              </li>
+              <li class="nav-item dropdown">
+                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                    {{ Auth::user()->name }}
+                </a>
+
+                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" href="{{ route('logout') }}"
+                       onclick="event.preventDefault();
+                                     document.getElementById('logout-form').submit();">
+                        {{ __('Logout') }}
+                    </a>
+
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                        @csrf
+                    </form>
+                </div>
+            </li>
             </ul>
           </div>
         </div>
